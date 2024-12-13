f969f5cafb868987e94378a68757e6fa5a9719b9
Arun VenuGopal <ewo73h24@students.codeinstitute.net>
2024-12-13 00:15:00 +0000

    queryset in views.py in app blog in Class PostList

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from .models import Post
 
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
+    # queryset = Post.objects.all()
     template_name = "post_list.html"
     
